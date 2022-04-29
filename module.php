6868526c363d18f6eb8da271e1a7478fd246c7d6
Sir Peter <77232439+reteP-riS@users.noreply.github.com>
2022-04-29 14:33:06 +0200

    Bumped version to 1.0.14.

--- module.php
+++ module.php
@@ -42,7 +42,7 @@ return new class extends AbstractModule implements ModuleCustomInterface, Module
     
     public const CUSTOM_WEBSITE = 'https://github.com/reteP-riS/HistoricEvents-Germany';
     
-    public const CUSTOM_VERSION = '1.0.13';
+    public const CUSTOM_VERSION = '1.0.14';
 
     public const CUSTOM_LAST = 'https://github.com/reteP-riS/HistoricEvents-Germany/raw/main/latest-version.txt';
 
